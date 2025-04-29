05d094497640cf668fdc96b1102b12cc2de88d1f
Joao Vitor <githubconta2021@gmail.com>
2025-04-29 10:07:53 -0300

    Description: CONSULTA PARA VERIFICAR QUANTIDADE DE STATUS PROFISSIONAL

--- Código_sql/codeSQL.sql
+++ Código_sql/codeSQL.sql
@@ -17,3 +17,24 @@ LEFT JOIN temp_tables.ibge_genders AS ibge
      ON LOWER(cus.first_name) = LOWER(ibge.first_name)
 GROUP BY ibge.gender; 
 
+-- (Query 2) Status profissional dos leads
+-- Colunas: status profissional, leads (%)
+
+SELECT 
+     CASE 
+	     WHEN professional_status = 'freelancer' THEN 'freelancer'
+		 WHEN professional_status =  'retired' THEN 'aposentado'
+		 WHEN professional_status =  'clt' THEN 'clt'
+		 WHEN professional_status = 'other' THEN 'outros'
+		 WHEN professional_status = 'civil_servant' THEN 'Funcionário(a) público(a)'
+		 WHEN professional_status = 'student ' THEN 'estudante'
+		 WHEN professional_status = 'self_employed' THEN 'autônomo'
+		 WHEN professional_status = 'busine ' THEN 'Empressário'
+		 WHEN professional_status = 'businessman' THEN 'Empressário'
+		 END AS "status profissional", 
+		 (COUNT(*)::float) /(SELECT COUNT(*) FROM sales.customers) AS "Leads %"
+FROM sales.customers
+GROUP BY professional_status
+ORDER BY "Leads %"; 
+
+
