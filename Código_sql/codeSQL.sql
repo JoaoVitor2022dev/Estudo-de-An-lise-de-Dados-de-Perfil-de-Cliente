8041c885cf2f818d5a92842002213e74855d4d84
Joao Vitor <githubconta2021@gmail.com>
2025-05-05 10:31:18 -0300

    Description: CONSULTA faixa salarioa baseado na ledas e ordenada

--- Código_sql/codeSQL.sql
+++ Código_sql/codeSQL.sql
@@ -53,3 +53,24 @@ GROUP BY "Faixa etária"
 ORDER BY "Faixa etária" DESC;  
 
 
+-- (Query 4) Faixa salarial dos leads
+-- Colunas: faixa salarial, leads (%), ordem
+
+SELECT 
+      CASE 
+	      WHEN income < 5000 THEN '0-5000'
+		  WHEN income < 10000 THEN '5000-10000'
+		  WHEN income < 15000 THEN '10000-15000'
+		  WHEN income < 20000 THEN '15000-20000'
+		  ELSE '20000+' END "Faixa Salarial", 
+		  COUNT(*)::float/(SELECT COUNT(*) FROM sales.customers) AS "leads (%)",
+      CASE 
+	      WHEN income < 5000 THEN 1
+		  WHEN income < 10000 THEN 2
+		  WHEN income < 15000 THEN 3
+		  WHEN income < 20000 THEN 4
+		  ELSE 5 END "Ordem"
+FROM sales.customers
+GROUP BY "Faixa Salarial", "Ordem"
+ORDER BY "Ordem" DESC;  
+
